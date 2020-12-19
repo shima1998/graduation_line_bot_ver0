633403d4ae48f49d89c108e9584a836a855510cb
shima1998 <b1017216@gmail.com>
2020-12-19 15:23:10 +0900

    リモートでやるためにプッシュ

--- line_bot0.rb
+++ line_bot0.rb
@@ -3,54 +3,24 @@
 
 require "cgi"
 require "uri"
+require "json"
 
-#data = CGI.new()
-
-# s = STDIN.read
-
-
-post = $stdin.read
-
-###########################################################################################
-# WebhookURLのverify時に送られるJSONは{"events":[],"destination":"任意のdestination"}
-# 
-# 文章が送られてきたときのデータは改行なしで、JSON形式
-# {
-# "events":[{
-#     "type":"message",
-#     "replyToken":"任意のリプライトークン",
-#     "source":{
-#         "userId":"ユーザーのID",
-#         "type":"user"
-#         },
-#     "timestamp":任意のタイムスタンプ(int),
-#     "mode":"active",
-#     "message":{
-#         "type":"text",
-#         "id":"任意のID",
-#         "text":"ふぁふぁふぁ"
-#         }
-#     }],
-# "destination":"任意のdestination"
-# }
-###########################################################################################
-
+data = CGI.new()
 
 a = "aaaaaaa"
 
-p = post.to_s
+# p = post.to_s
 
 #d = data
-
-
-
 
 print "Content-type: text/html\n\n"
 
 print a
+print "<br>"
+print data['book_name'].to_s
 
 f =File.open("test_stdin.txt", "w")
-f.write(p)  # ファイルに書き込む
+f.write(data['destination'].to_s)  # ファイルに書き込む
 f.close
    
 # print "<br>"
@@ -78,3 +48,53 @@ f.close
 # print "<br>"
 # #print URI.decode_www_form(p)
 # print "<br>"
+
+# s = STDIN.read
+
+
+# post = $stdin.read
+
+###########################################################################################
+# POSTで送られてきたJSONをまんまtxtに入れてみた結果                                          #
+###########################################################################################
+# WebhookURLのverify時に送られるJSONは
+# {"events":[],"destination":"任意のdestination"}
+# 
+# 文章が送られてきたときのデータは改行なしで、JSON形式
+# {
+# "events":[{
+#     "type":"message",
+#     "replyToken":"任意のリプライトークン",
+#     "source":{
+#         "userId":"ユーザーのID",
+#         "type":"user"
+#         },
+#     "timestamp":任意のタイムスタンプ(int),
+#     "mode":"active",
+#     "message":{
+#         "type":"text",
+#         "id":"任意のID",
+#         "text":"ふぁふぁふぁ"
+#         }
+#     }],
+# "destination":"任意のdestination"
+# }
+###########################################################################################
+# 返信する際のテンプレートのJSON
+# 
+# 'Content-Type: application/json' \
+# 'Authorization: Bearer {channel access token}' \
+# '{
+#     "replyToken":"nHuyWiB7yP5Zw52FIkcQobQuGDXCTA",
+#     "messages":[
+#         {
+#             "type":"text",
+#             "text":"Hello, user"
+#         },
+#         {
+#             "type":"text",
+#             "text":"May I help you?"
+#         }
+#     ]
+# }'
+###########################################################################################
